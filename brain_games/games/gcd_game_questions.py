4a9582725d02c13be1eac429bb8299cfe5cf42c9
shafigullinik <shafigullin.ik@gmail.com>
2019-11-02 08:45:35 +0300

    linter is happy now

--- brain_games/games/gcd_game_questions.py
+++ brain_games/games/gcd_game_questions.py
@@ -5,12 +5,12 @@ def create_question():
     max_int = 100
     min_int = 0
     number1 = random.randint(min_int, max_int)
-    number2 = random.randint(min_int, max_int)    
+    number2 = random.randint(min_int, max_int)
     question = "{} {} =".format(number1, number2)
-    correct_answer = str(find_gcd((number1, number2)))    
+    correct_answer = str(find_gcd((number1, number2)))
     return(question, correct_answer)
-    
-    
+
+
 def find_gcd(pair):
     (first, second) = pair
     if second > first:
