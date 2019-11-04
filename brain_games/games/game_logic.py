b9d81bf25fe802ff94c91238e79ebfcd26623b48
shafigullinik <shafigullin.ik@gmail.com>
2019-11-04 10:20:37 +0300

    game_logic file is refactored

--- brain_games/games/game_logic.py
+++ brain_games/games/game_logic.py
@@ -9,19 +9,25 @@ import brain_games.games.prime_game_questions as prime_g_q
 def game(game_name):
     counter = 0
     while counter < 3:
-        if game_name == "even_game":
-            question_with_answer = e_g_q.create_question()
-        elif game_name == "calc_game":
-            question_with_answer = c_g_q.create_question()
-        elif game_name == "gcd_game":
-            question_with_answer = gcd_g_q.create_question()
-        elif game_name == "progression_game":
-            question_with_answer = p_g_q.create_question()
-        elif game_name == "prime_game":
-            question_with_answer = prime_g_q.create_question()
+        question_with_answer = create_question(game_name)        
         is_answer_correct = cli.ask_question(question_with_answer)
         if is_answer_correct is True:
             counter = counter + 1
         else:
             return False
     return True
+
+
+def create_question(game_name):
+    question_with_answer = ""
+    if game_name == "even_game":
+        question_with_answer = e_g_q.create_question()
+    if game_name == "calc_game":
+        question_with_answer = c_g_q.create_question()
+    if game_name == "gcd_game":
+        question_with_answer = gcd_g_q.create_question()
+    if game_name == "progression_game":
+        question_with_answer = p_g_q.create_question()
+    if game_name == "prime_game":
+        question_with_answer = prime_g_q.create_question()
+    return question_with_answer
